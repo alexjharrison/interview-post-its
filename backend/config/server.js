9a49e001f756bcfd2e520972e6c8c8f7412a6d2a
Alex Harrison <alexjharrison@gmail.com>
2020-11-17 22:37:33 -0500

    setup strapi models

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fb216ffbfff305c18df7b2391540e018'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7ed22305-7f83-423b-bb8c-7045d135cf39'
+};
